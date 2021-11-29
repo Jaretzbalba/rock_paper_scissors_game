fe56068e10477b7583f9b4082138ca6c66a1415f
Jaret Balba <Jaretzbalba@gmail.com>
2021-11-29 13:14:00 -0800

    created a single round function

--- computerPlay.js
+++ computerPlay.js
@@ -1,16 +1,15 @@
 let outcomes = Array("Rock", "Paper", "Scissors");
 
+let outcome;
+
 function computerPlay() {
     if (Math.floor(Math.random()*outcomes.length) === 0) {
-        let outcome = outcomes[0];
-        alert(outcomes[0]);
+        return outcome = outcomes[0]
     }
     else if (Math.floor(Math.random()*outcomes.length) === 1) {
-        let outcome = outcomes[1];
-        alert(outcomes[1]);
+        return outcome = outcomes[1]
     }
     else {
-        let outcome = outcomes[2];
-        alert(outcomes[2]);
+        return outcome = outcomes[2]
     }
 }
